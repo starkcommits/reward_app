a03f1c6eaaad233b224eda5bac629fcf73f0b38e
starkcommits <tinkal.kumar@xettle.net>
2025-07-09 12:48:43 +0000

    added report

--- rewardapp/reward_app/doctype/tds_report/tds_report.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, xFer India100x pvt ltd. and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class TDSReport(Document):
-	pass
